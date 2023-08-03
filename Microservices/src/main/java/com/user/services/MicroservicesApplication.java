d70a19ef7845c9dd586ab0b268e74462f920d005
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-08-03 18:00:22 +0530

    Update MicroservicesApplication.java

--- Microservices/src/main/java/com/user/services/MicroservicesApplication.java
+++ Microservices/src/main/java/com/user/services/MicroservicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesApplication.class,args);
+		SpringApplication. run(MicroservicesApplication.class,args);
 	}
 
 }
