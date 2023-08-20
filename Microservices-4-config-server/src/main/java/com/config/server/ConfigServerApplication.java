1e0394554f5e7e656f4bee149d459004bbca1f31
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-08-21 00:28:54 +0530

    Update ConfigServerApplication.java

--- Microservices-4-config-server/src/main/java/com/config/server/ConfigServerApplication.java
+++ Microservices-4-config-server/src/main/java/com/config/server/ConfigServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
